336b764eeb3c22b85c4c2e4568c47f70a02d8d7d
Amadou Latyr Ngom <amlatyr@gmail.com>
2019-05-04 14:40:52 -0400

    Reduce folder size

---
+++ gen_caption_data.py
@@ -0,0 +1,105 @@
+from __future__ import division
+
+from models import *
+from utils.utils import *
+from utils.datasets import *
+from pickle import dump
+
+
+import os
+import sys
+import time
+import datetime
+import argparse
+
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib.ticker import NullLocator
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--image_folder', type=str, default='data/samples', help='path to dataset')
+parser.add_argument('--config_path', type=str, default='config/yolov3.cfg', help='path to model config file')
+parser.add_argument('--weights_path', type=str, default='weights/yolov3.weights', help='path to weights file')
+parser.add_argument('--class_path', type=str, default='data/coco.names', help='path to class label file')
+parser.add_argument('--conf_thres', type=float, default=0.8, help='object confidence threshold')
+parser.add_argument('--nms_thres', type=float, default=0.4, help='iou thresshold for non-maximum suppression')
+parser.add_argument('--batch_size', type=int, default=1, help='size of the batches')
+parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
+parser.add_argument('--img_size', type=int, default=416, help='size of each image dimension')
+parser.add_argument('--use_cuda', type=bool, default=True, help='whether to use cuda if available')
+parser.add_argument('--gen_data', type=str, default="train", help='whether to generate training or testing data')
+opt = parser.parse_args()
+print(opt)
+
+cuda = torch.cuda.is_available() and opt.use_cuda
+
+os.makedirs('output', exist_ok=True)
+
+# Set up model
+model = Darknet(opt.config_path, img_size=opt.img_size)
+model.load_weights(opt.weights_path)
+
+if cuda:
+    model.cuda()
+
+model.eval() # Set in evaluation mode
+
+dataloader = DataLoader(ImageFolder(opt.image_folder, img_size=opt.img_size),
+                        batch_size=opt.batch_size, shuffle=False, num_workers=opt.n_cpu)
+
+classes = load_classes(opt.class_path) # Extracts class labels from file
+
+Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+imgs = []           # Stores image paths
+img_detections = [] # Stores detections for each image index
+
+print ('\nPerforming object detection:')
+prev_time = time.time()
+for batch_i, (img_paths, input_imgs) in enumerate(dataloader):
+    # Configure input
+    input_imgs = Variable(input_imgs.type(Tensor))
+
+    # Get detections
+    with torch.no_grad():
+        detections = model(input_imgs)
+        detections = non_max_suppression(detections, 80, opt.conf_thres, opt.nms_thres)
+
+
+    # Log progress
+    current_time = time.time()
+    inference_time = datetime.timedelta(seconds=current_time - prev_time)
+    prev_time = current_time
+    print ('\t+ Batch %d, Inference Time: %s' % (batch_i, inference_time))
+
+    # Save image and detections
+    imgs.extend(img_paths)
+    img_detections.extend(detections)
+
+
+print ('\nSaving Detected Objects:')
+file_objects = dict()
+object_coords = dict()
+# Iterate through images and save plot of detections
+for img_i, (path, detections) in enumerate(zip(imgs, img_detections)):
+
+    #print ("(%d) Image: '%s'" % (img_i, path))
+    file_id = path.split('/')[-1].split('.')[0]
+
+    # Draw bounding boxes and labels of detections
+    if detections is not None:
+        file_objects[file_id] = [classes[int(c[6])] for c in detections]
+        object_coords[file_id] = [c[:4] for c in detections]
+
+if (opt.gen_data=="train"):
+    dump(file_objects, open('detected_objects.pkl', 'wb'))
+    dump(object_coords, open('object_coords.pkl', 'wb'))
+else:
+    print(file_objects)
+    dump(file_objects, open('test_detected_objects.pkl', 'wb'))
+    dump(object_coords, open('test_object_coords.pkl', 'wb'))
